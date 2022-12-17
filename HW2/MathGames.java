ea7826663079044df543cd4b95ea6a680adcb47a
everettb0403 <113644554+everettb0403@users.noreply.github.com>
2022-12-16 21:25:26 -0500

    Update MathGames.java

--- HW2/MathGames.java
+++ HW2/MathGames.java
@@ -1,5 +1,6 @@
 package HW2;
 
+import javax.xml.stream.events.Namespace;
 
 public class MathGames {
     // 1. Create a method that will calculate the Pythagorean Theorem (https://en.wikipedia.org/wiki/Pythagorean_theorem)
@@ -13,10 +14,31 @@ public class MathGames {
 
      //method that will calculate the Pythagorean Theorem c = sqrt(a^2+b^2) ( needed help)
 
-    System.out.println("Hello" + " " + (a^2 + b^2));
 
-    public static void pythag (double a, double b))
-    double c = Math.sqrt(Math.pow(a, b:2) + math.pow(b, b:2)
+//Program : Pythagorean Theorem 
+
+public static void main(String[] args) {
+#include <iostream>
+#include <math.h>
+
+using Namespace std;
+
+void main ()
+
+{
+
+        float a, b, c;
+
+        cout << "Please input the values of a and b: ";
+        cin >> a >> b;
+
+        c = a*a + b*b;
+        c = sqrt(c) ; 
+
+        cout << The Hypotenuse of the triangle is: " << c << '.' <<endl;
+
+        system('Pause") ; 
+}}
     
     // 2. Create a method that will calcuate my grade in the class. You can use the grade range as follows
         // A: 100 - 90
@@ -30,7 +52,16 @@ public class MathGames {
         // Hint 2: You will probably need to cast the double to an int
         // Hint 3: You will probably need to use the Math.ceil() method 
 
+        Dim grade as decimals
 
+        grade = Val(txtMark.text)
+
+        If (grade>=90) Then lbl.Grade.Text - "A"
+        If (grade<=89) Then lbl.Grade.Text - "B"
+        If (grade<=79) Then lbl.Grade.Text - "C"
+        If (grade<=69.3) Then lbl.Grade.Text -"C-"
+        If (grade<=69) Then lbl.Grade.Text - "d"
+        If (grade<=60) Then lbl.Grade.Text - "F"
 
 
 
@@ -43,16 +74,15 @@ public class MathGames {
     //      Hint 4: You will probably need to use the Math.ceil() method 
 
     
- 
+    Dim amt, tipPercent, tipAmt As double
+    amt = txtBill.Text 
+    tipPercent = txtTipPercent.Text / 100
+    '$100 if tip percent was 18%
+    'What is 18% of $100
+    '18% * 100 
+    '18/100 * 100
+    '.18 * 100'
+    tipAmt = amt * tipPercent
+    txtTipAmount.Text = tip Amt 
 
-
-    // you do not need this main if you want to make a tester class
-     public static void main(String[] args){
-        System.out.println(
-            pythag(a:3 , b:4))
-    }
-
-
-
-
-}
+               
