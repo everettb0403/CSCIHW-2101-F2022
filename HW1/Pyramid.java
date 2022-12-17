8600ffe8ab76094c318a6c482f6d28bdbe1e7aef
everettb0403 <113644554+everettb0403@users.noreply.github.com>
2022-12-16 20:32:51 -0500

    Update Pyramid.java

--- HW1/Pyramid.java
+++ HW1/Pyramid.java
@@ -15,6 +15,19 @@ Hint2: Also notice the method is void meaning you will not be returning anything
 */
 public class Pyramid {
     public static void Output() {
-        // Put your code here
-    }
-}
+    
+        
+        int main() {
+        
+            int rows;
+        
+            cout << "9: ";
+            cin >> rows;
+        
+            for(int i = 1; i <= rows; ++i) {
+                for(int j = 1; j <= i; ++j) {
+                    cout << "# ";
+                }
+                cout << "\n";
+            }
+            return 0;
